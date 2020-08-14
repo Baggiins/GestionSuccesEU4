9d2c8155929f9ebe6b50f0b78aeabf720aea697f
Baggiins <jrmduarte29@gmail.com>
2020-08-14 17:54:03 +0200

    FirstCommit

---
+++ src/main/java/com/baggiins/europauniv/succes/SuccesApplication.java
@@ -0,0 +1,13 @@
+package com.baggiins.europauniv.succes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuccesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baggiins/europauniv/succes/SuccesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baggiins.europauniv.succes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuccesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
